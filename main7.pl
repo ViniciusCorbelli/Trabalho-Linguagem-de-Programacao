96a370477314c7feb6a8ff1d28ce92a34b67c4a3
vinicius.corbelli <vinicius.corbelli@bling.com.br>
2023-05-07 00:17:28 -0300

    feat: interface

--- main7.pl
+++ main7.pl
@@ -4,6 +4,14 @@
 :- use_module(library(clpfd)).
 :- use_module(library(pce)).
 
+:- dynamic tabuleiro/1.
+:- dynamic altura/1.
+
+% PREDICADOS GERAIS DO PROBLEMA
+% estabelece simbolos aceitos
+simbolo('X').
+simbolo('O').
+
 mostra_aviso(Mensagem) :-
     new(Janela, dialog('Alerta')),
     send(Janela, append, new(Texto, text(Mensagem))),
@@ -33,91 +41,113 @@ jogar :-
     send(Menu, open).
 
 jogo_normal(Altura) :-
+    assertz(altura(Altura)),
     Largura is Altura + 1,
     Consecutivas is Altura,
-    cria_tabuleiro(Altura, Largura, Tabuleiro),
-    imprime_tabuleiro_v1(Altura, Largura, Consecutivas, Tabuleiro).
 
+    cria_tabuleiro(Altura, Largura),
+    imprime_tabuleiro_v1.
+    
 jogo_simplificada(Altura) :-
+    assertz(altura(Altura)),
     Largura is Altura + 1,
     Consecutivas is Altura,
-    cria_tabuleiro(Altura, Largura, Tabuleiro),
-    imprime_tabuleiro_v2(Altura, Largura, Consecutivas, Tabuleiro).
 
-% PREDICADOS GERAIS DO PROBLEMA
-% estabelece simbolos aceitos
-simbolo('X').
-simbolo('O').
+    cria_tabuleiro(Altura, Largura),
+    imprime_tabuleiro_v2.
+    
+% Predicado para adicionar o tabuleiro na dynamic
+adicionar_tabuleiro(Tabuleiro) :-
+    retractall(tabuleiro(_)),
+    assertz(tabuleiro(Tabuleiro)).
 
 % Predicado para criar tabuleiro
-cria_tabuleiro(Largura, Altura, Tabuleiro) :-
+cria_tabuleiro(Largura, Altura) :-
     length(Tabuleiro, Largura),
-    maplist(length_(Altura), Tabuleiro).
+    maplist(length_(Altura), Tabuleiro),
+    adicionar_tabuleiro(Tabuleiro).
 length_(Altura, Tabuleiro) :-
     length(Tabuleiro, Altura),
     maplist(nulo, Tabuleiro).
 nulo('N').
+    
+imprime_tabuleiro_v1 :-
+    altura(Altura),
+    Largura is Altura + 1,
+    Consecutivas is Altura,
 
-imprime_tabuleiro_v1(Altura, Largura, Consecutivas, Tabuleiro) :-
     new(Window, dialog('Jogo da Velha', size(Altura * 50, Largura * 50))),
-    cria_botao_tabuleiro_v1(1, 1, Altura, Largura, Consecutivas, Tabuleiro, Window),
+    cria_botao_tabuleiro_v1(1, 1, Altura, Largura, Window),
     send(Window, open).
-
+    
 % Predicado auxiliar para criar o botão de uma posição
-cria_botao_posicao_v1(X, Y, Janela, Tabuleiro, Altura, Largura, Consecutivas) :-
-    get_posicao(X, Y, Tabuleiro, Simbolo),
-
-    new(Botao, button(Simbolo, and(message(@prolog, joga_v1, Altura, Largura, Consecutivas, Tabuleiro, X, Y),
-                                    message(@prolog, fechar_menu, Janela)))),
+cria_botao_posicao_v1(X, Y, Janela, Altura, Largura) :-
+    get_posicao(X, Y, Simbolo),
+    
+    new(Botao, button(Simbolo, and(message(@prolog, joga_v1, X, Y),
+                                message(@prolog, fechar_menu, Janela)))),
     send(Janela, append, Botao).
-
+    
 % Predicado auxiliar para criar os botões de uma linha
-cria_botao_linha_v1(X, Y, Altura, Largura, Consecutivas, Janela, Tabuleiro) :-
-    cria_botao_posicao_v1(X, Y, Janela, Tabuleiro, Altura, Largura, Consecutivas),
+cria_botao_linha_v1(X, Y, Altura, Largura, Janela) :-
+    cria_botao_posicao_v1(X, Y, Janela, Altura, Largura),
     Y1 is Y + 1,
     (Y1 =< Largura ->
-    cria_botao_linha_v1(X, Y1, Altura, Largura, Consecutivas, Janela, Tabuleiro)
+    cria_botao_linha_v1(X, Y1, Altura, Largura, Janela)
     ; send(Janela, append, new(Linea, text('\n')))).
-
+    
 % Predicado para criar os botões de todas as linhas
-cria_botao_tabuleiro_v1(X, _, Altura, _, _, _, _) :- X > Altura, !.
-cria_botao_tabuleiro_v1(X, Y, Altura, Largura, Consecutivas, Tabuleiro, Janela) :-
-    cria_botao_linha_v1(X, Y, Altura, Largura, Consecutivas, Janela, Tabuleiro),
+cria_botao_tabuleiro_v1(X, _, Altura, _, _) :- X > Altura, !.
+cria_botao_tabuleiro_v1(X, Y, Altura, Largura, Janela) :-
+    cria_botao_linha_v1(X, Y, Altura, Largura, Janela),
     X1 is X + 1,
-    cria_botao_tabuleiro_v1(X1, Y, Altura, Largura, Consecutivas, Tabuleiro, Janela).
+    cria_botao_tabuleiro_v1(X1, Y, Altura, Largura, Janela).
 
+imprime_tabuleiro_v2 :-
+    altura(Altura),
+    Largura is Altura + 1,
+    Consecutivas is Altura,
 
-imprime_tabuleiro_v2(Altura, Largura, Consecutivas, Tabuleiro) :-
     new(Window, dialog('Jogo da Velha', size(Altura * 50, Largura * 50))),
-    cria_botao_tabuleiro_v2(1, 1, Altura, Largura, Consecutivas, Tabuleiro, Window),
+    cria_botao_tabuleiro_v2(1, 1, Altura, Largura, Window),
     send(Window, open).
-
+    
 % Predicado auxiliar para criar o botão de uma posição
-cria_botao_posicao_v2(X, Y, Janela, Tabuleiro, Altura, Largura, Consecutivas) :-
-    get_posicao(X, Y, Tabuleiro, Simbolo),
-
-    new(Botao, button(Simbolo, and(message(@prolog, joga_v2, Altura, Largura, Consecutivas, Tabuleiro, Y),
-                                    message(@prolog, fechar_menu, Janela)))),
+cria_botao_posicao_v2(X, Y, Janela, Altura, Largura) :-
+    get_posicao(X, Y, Simbolo),
+    
+    new(Botao, button(Simbolo, and(message(@prolog, joga_v2, Y),
+                                message(@prolog, fechar_menu, Janela)))),
     send(Janela, append, Botao).
-
+    
 % Predicado auxiliar para criar os botões de uma linha
-cria_botao_linha_v2(X, Y, Altura, Largura, Consecutivas, Janela, Tabuleiro) :-
-    cria_botao_posicao_v2(X, Y, Janela, Tabuleiro, Altura, Largura, Consecutivas),
+cria_botao_linha_v2(X, Y, Altura, Largura, Janela) :-
+    cria_botao_posicao_v2(X, Y, Janela, Altura, Largura),
     Y1 is Y + 1,
     (Y1 =< Largura ->
-    cria_botao_linha_v2(X, Y1, Altura, Largura, Consecutivas, Janela, Tabuleiro)
+    cria_botao_linha_v2(X, Y1, Altura, Largura, Janela)
     ; send(Janela, append, new(Linea, text('\n')))).
-
+    
 % Predicado para criar os botões de todas as linhas
-cria_botao_tabuleiro_v2(X, _, Altura, _, _, _, _) :- X > Altura, !.
-cria_botao_tabuleiro_v2(X, Y, Altura, Largura, Consecutivas, Tabuleiro, Janela) :-
-    cria_botao_linha_v2(X, Y, Altura, Largura, Consecutivas, Janela, Tabuleiro),
+cria_botao_tabuleiro_v2(X, _, Altura, _, _) :- X > Altura, !.
+cria_botao_tabuleiro_v2(X, Y, Altura, Largura, Janela) :-
+    cria_botao_linha_v2(X, Y, Altura, Largura, Janela),
     X1 is X + 1,
-    cria_botao_tabuleiro_v2(X1, Y, Altura, Largura, Consecutivas, Tabuleiro, Janela).
+    cria_botao_tabuleiro_v2(X1, Y, Altura, Largura, Janela).
 
 
 % Predicado para buscar o símbolo de uma posição do tabuleiro
-get_posicao(X, Y, Tabuleiro, Simbolo) :-
+get_posicao(X, Y, Simbolo) :-
+    tabuleiro(Tabuleiro),
+    nth1(X, Tabuleiro, Linha),
+    nth1(Y, Linha, Simbolo),
+    (var(Simbolo) -> Simbolo = 'N' ; true).
+
+/* TODO: V2*/
+
+% Predicado para buscar o símbolo de uma posição do tabuleiro
+get_posicao(X, Y, Simbolo) :-
+    tabuleiro(Tabuleiro),
     nth1(X, Tabuleiro, Linha),
     nth1(Y, Linha, Simbolo),
     (var(Simbolo) -> Simbolo = 'N' ; true).
@@ -140,106 +170,129 @@ troca_linha([Coluna|Colunas], NumColuna, Simbolo, [Coluna|NovasColunas]) :-
 % PREDICADOS PARA A VERSAO 1 DO JOGO DA VELHA
 
 % Predicado que tenta buscar simbolo na posicao m x n do tabuleiro
-verifica_simbolo(Tabuleiro, Linha, Coluna):-
+verifica_simbolo(Linha, Coluna):-
+    tabuleiro(Tabuleiro),
     nth1(Linha, Tabuleiro, LinhaTabuleiro),
     nth1(Coluna, LinhaTabuleiro, PosSim),
     not(simbolo(PosSim)).
 
 % Predicado para fazer jogada
-joga_v1(Altura, Largura, Consecutivas, Tabuleiro, Linha, Coluna) :-
-    mostra_aviso('0'),
-    (verifica_simbolo(Tabuleiro, Linha, Coluna)) ->
-    mostra_aviso('1'),
+joga_v1(Linha, Coluna) :-
+    tabuleiro(Tabuleiro),
+
+    (verifica_simbolo(Linha, Coluna)) ->
     troca(Tabuleiro, Linha, Coluna, NovoTabuleiro, 'X'),
-    not(vitoria_horizontal(NovoTabuleiro, Linha, 'X', 1)),
-    not(vitoria_vertical(NovoTabuleiro, Coluna, 'X', 1)),
-    not(vitoria_diagonal(NovoTabuleiro, 'X', 1, Largura)),
-    joga2_v1(Altura, Largura, Consecutivas, NovoTabuleiro);
+    adicionar_tabuleiro(NovoTabuleiro),
+    not(vitoria_horizontal(Linha, 'X', 1)),
+    not(vitoria_vertical(Coluna, 'X', 1)),
+    not(vitoria_diagonal('X', 1)),
+    empate,
+    joga2_v1;
     mostra_aviso('Posicao ja preenchida, escolha outra'),
-    imprime_tabuleiro_v1(Altura, Largura, Consecutivas, Tabuleiro).
+    imprime_tabuleiro_v1.
 
 % Jogador 2 joga
-joga2_v1(Altura, Largura, Consecutivas, Tabuleiro) :-
+joga2_v1 :-
+    tabuleiro(Tabuleiro),
+
     % Encontra a primeira posição vazia em que o jogador 2 pode jogar e vencer
-    jogada_vencedora(Tabuleiro, Altura, Largura, Consecutivas, Linha, Coluna),
+    jogada_vencedora(Linha, Coluna),
     troca(Tabuleiro, Linha, Coluna, NovoTabuleiro, 'O'),
-    not(vitoria_horizontal(NovoTabuleiro, Linha, 'O', 2)),
-    not(vitoria_vertical(NovoTabuleiro, Coluna, 'O', 2)),
-    not(vitoria_diagonal(NovoTabuleiro, 'O', 2, Largura)),
-    imprime_tabuleiro_v1(Altura, Largura, Consecutivas, NovoTabuleiro).
+    adicionar_tabuleiro(NovoTabuleiro),
+    not(vitoria_horizontal(Linha, 'O', 2)),
+    not(vitoria_vertical(Coluna, 'O', 2)),
+    not(vitoria_diagonal('O', 2)),
+    empate,
+    imprime_tabuleiro_v1.
 
 % PREDICADOS PARA A VERSAO 2
 
-procura_indice(Tabuleiro, Coluna, Indice) :-
+procura_indice(Coluna, Indice) :-
+    tabuleiro(Tabuleiro),
+
     nth0(Coluna, Tabuleiro, ColunaEscolhida),
     reverse(ColunaEscolhida, ColunaInvertida),
     length(ColunaInvertida, Tam),
     nth0(Aux_Indice, ColunaInvertida, 'N'),
     Indice is Tam - Aux_Indice.
 
-troca_v2(Tabuleiro, Coluna, Simbolo, NovoTabuleiro, Indice) :-
+troca_v2(Coluna, Simbolo, NovoTabuleiro, Indice) :-
+    tabuleiro(Tabuleiro),
+    
     Coluna1 is Coluna - 1,
     transpose(Tabuleiro, Transposta),
-    procura_indice(Transposta, Coluna1, Indice),
+    procura_indice(Coluna1, Indice),
     troca(Tabuleiro, Indice, Coluna, NovoTabuleiro, Simbolo).
 
+verifica_coluna(Coluna) :-
+    tabuleiro(Tabuleiro),
 
-verifica_coluna(Tabuleiro, Coluna) :-
     transpose(Tabuleiro, Transposta),
     Coluna1 is Coluna - 1,
     nth0(Coluna1, Transposta, ColunaEscolhida),
     member('N', ColunaEscolhida).
 
 % Predicado para fazer jogada
-joga_v2(Altura, Largura, Consecutivas, Tabuleiro, Coluna) :-
-    (verifica_simbolo(Tabuleiro, Linha, Coluna)) ->
-    troca_v2(Tabuleiro, Coluna, 'X', NovoTabuleiro, Indice),
-    not(vitoria_horizontal(NovoTabuleiro, Linha, 'X', 1)),
-    not(vitoria_vertical(NovoTabuleiro, Coluna, 'X', 1)),
-    not(vitoria_diagonal(NovoTabuleiro, 'X', 1, Largura)),
-    joga2_v2(Altura, Largura, Consecutivas, NovoTabuleiro);
+joga_v2(Coluna) :-
+    (verifica_coluna(Coluna)) ->
+    troca_v2(Coluna, 'X', NovoTabuleiro, Linha),
+    adicionar_tabuleiro(NovoTabuleiro),
+    not(vitoria_horizontal(Linha, 'X', 1)),
+    not(vitoria_vertical(Coluna, 'X', 1)),
+    not(vitoria_diagonal('X', 1)),
+    empate,
+    joga2_v2;
     mostra_aviso('Coluna selecionada esta cheia, selecione outra'),
-    imprime_tabuleiro_v2(Altura, Largura, Consecutivas, Tabuleiro).
+    imprime_tabuleiro_v2(Tabuleiro).
 
 % Jogador 2 joga
-joga2_v2(Altura, Largura, Consecutivas, Tabuleiro) :-
+joga2_v2 :-
+    tabuleiro(Tabuleiro),
+
     % Encontra a primeira posição vazia em que o jogador 2 pode jogar e vencer
-    jogada_vencedora_v2(Tabuleiro, Altura, Largura, Consecutivas, Linha, Coluna),
-    troca(Tabuleiro, Linha, Coluna, NovoTabuleiro, 'O'),
-    not(vitoria_horizontal(NovoTabuleiro, Linha, 'O', 2)),
-    not(vitoria_vertical(NovoTabuleiro, Coluna, 'O', 2)),
-    not(vitoria_diagonal(NovoTabuleiro, 'O', 2, Largura)),
-    imprime_tabuleiro_v2(Altura, Largura, Consecutivas, NovoTabuleiro).
+    jogada_vencedora_v2(Linha, Coluna),
+    troca_v2(Coluna, 'O', NovoTabuleiro, Linha),
+    adicionar_tabuleiro(NovoTabuleiro),
+    not(vitoria_horizontal(Linha, 'O', 2)),
+    not(vitoria_vertical(Coluna, 'O', 2)),
+    not(vitoria_diagonal('O', 2)),
+    empate,
+    imprime_tabuleiro_v2.
 
 % PREDICADOS PARA DETERMINAR VENCEDOR
 
 % Verifica vencedor na horizontal
-vitoria_horizontal(Tabuleiro, Linha, Simbolo, Jogador) :-
+vitoria_horizontal(Linha, Simbolo, Jogador) :-
+    tabuleiro(Tabuleiro),
+
     Linha1 is Linha - 1,
     nth0(Linha1, Tabuleiro, LinhaTeste),
     mesmo_simbolo(LinhaTeste, Simbolo),
-    mostra_aviso(concat_atom(['Vitoria do jogador:', Jogador])),
-    jogar.
+    mostra_aviso(Jogador).
 
 % Verifica vencedor na vertical
-vitoria_vertical(Tabuleiro, Coluna, Simbolo, Jogador) :-
+vitoria_vertical(Coluna, Simbolo, Jogador) :-
+    tabuleiro(Tabuleiro),
     Coluna1 is Coluna - 1,
+
     transpose(Tabuleiro, Transposta),
     nth0(Coluna1, Transposta, LinhaTeste),
     mesmo_simbolo(LinhaTeste, Simbolo),
-    mostra_aviso(concat_atom(['Vitoria do jogador:', Jogador])),
-    jogar.
+    mostra_aviso(Jogador).
 
 % Verifica vencedor nas diagonais
-vitoria_diagonal(Tabuleiro, Simbolo, Jogador, Largura):-
+vitoria_diagonal(Simbolo, Jogador):-
+    tabuleiro(Tabuleiro),
+    altura(Altura),
+    Largura is Altura + 1,
+    Largura1 is Largura - 1,
+
     pega_diagonal1(Tabuleiro, 1, Diagonal1),
     pega_diagonal1(Tabuleiro, 2, Diagonal2),
     pega_diagonal2(Tabuleiro, Largura, Diagonal3),
-    Largura1 is Largura - 1,
     pega_diagonal2(Tabuleiro, Largura1, Diagonal4),
     (mesmo_simbolo(Diagonal1, Simbolo); mesmo_simbolo(Diagonal2, Simbolo); mesmo_simbolo(Diagonal3, Simbolo) ; mesmo_simbolo(Diagonal4, Simbolo)) ->
-    mostra_aviso(concat_atom(['Vitoria do jogador:', Jogador])),
-    jogar;
+    mostra_aviso(Jogador);
     fail.
 
 % Predicado que pega a diagonal principal mais a diagonal que comeca em
@@ -266,11 +319,14 @@ pega_diagonal_aux2([Linha|OutrasLinhas], Indice, [Elemento|DiagonalRestante]) :-
 
 
 % Verifica empate se nao houver qualquer 'N' no tabuleiro
-empate(Tabuleiro) :-
+empate :-
+    tabuleiro(Tabuleiro),
+
     converte_tabuleiro(Tabuleiro, Lista),
     not(member('N', Lista)) ->
     mostra_aviso('A partida empatou'),
     jogar;
+    write('').
 
 % Converte o tabuleiro m x m mais 1 por uma lista simples
 converte_tabuleiro(Tabuleiro, Lista_Tabuleiro):-
@@ -286,15 +342,19 @@ converte_tabuleiro([Tabuleiro|Resto], AcumuladorParcial, Lista_Tabuleiro) :-
 mesmo_simbolo([],_).
 mesmo_simbolo([H|T], H) :- mesmo_simbolo(T,H).
 
-jogada_vencedora(Tabuleiro, Altura, Largura, Consecutivas, Linha, Coluna) :-
+jogada_vencedora(Linha, Coluna) :-
+    tabuleiro(Tabuleiro),
+    altura(Altura),
+    Largura is Altura + 1,
+
     findall([L, C],
             (between(1, Altura, L),
              between(1, Largura, C),
-             verifica_simbolo(Tabuleiro, L, C),
+             verifica_simbolo(L, C),
              troca(Tabuleiro, L, C, NovoTabuleiro, 'O'),
              (vitoria_horizontal_aux(NovoTabuleiro, L, 'O', 2);
               vitoria_vertical_aux(NovoTabuleiro, C, 'O', 2);
-              vitoria_diagonal(NovoTabuleiro, 'O', 2, Largura))),
+              vitoria_diagonal_aux(NovoTabuleiro, 'O', C, L, Consecutivas, Altura, Largura))),
             JogadasVitoria),
     (   member([Linha, Coluna], JogadasVitoria),
         ! % se houver uma jogada que leva à vitória imediata, retorne essa jogada
@@ -304,13 +364,13 @@ jogada_vencedora(Tabuleiro, Altura, Largura, Consecutivas, Linha, Coluna) :-
         findall([L, C],
             (between(1, Altura, L),
              between(1, Largura, C),
-             verifica_simbolo(Tabuleiro, L, C)),
+             verifica_simbolo(L, C)),
             JogadasValidas),
 
         findall([L, C, P],
                 (member([L, C], JogadasValidas),
                  troca(Tabuleiro, L, C, NovoTabuleiro, 'O'),
-                 pontuacao_jogada(NovoTabuleiro, Altura, Largura, Consecutivas, L, C, JogadasValidas, P)),
+                 pontuacao_jogada(NovoTabuleiro, L, C, JogadasValidas, P)),
                 JogadasPontuadas),
                 
         sort(JogadasPontuadas, SortedJogadasPontuadas),
@@ -318,14 +378,19 @@ jogada_vencedora(Tabuleiro, Altura, Largura, Consecutivas, Linha, Coluna) :-
         member([Linha, Coluna, _], ReverseJogadasPontuadas)
     ).
 
-jogada_vencedora_v2(Tabuleiro, Altura, Largura, Consecutivas, Linha, Coluna) :-
+jogada_vencedora_v2(Linha, Coluna) :-
+    tabuleiro(Tabuleiro),
+    altura(Altura),
+    Largura is Altura + 1,
+    Consecutivas is Altura + 1,
+    
     findall([C],
             (between(1, Largura, C),
-             verifica_coluna(Tabuleiro, C),
-             troca_v2(Tabuleiro, C, 'O', NovoTabuleiro, L),
+             verifica_coluna(C),
+             troca_v2(C, 'O', NovoTabuleiro, L),
              (vitoria_horizontal_aux(NovoTabuleiro, L, 'O', 2);
               vitoria_vertical_aux(NovoTabuleiro, C, 'O', 2);
-              vitoria_diagonal(NovoTabuleiro, 'O', 2, Largura))),
+              vitoria_diagonal_aux(NovoTabuleiro, 'O', C, L, Consecutivas, Altura, Largura))),
             JogadasVitoria),
     (   member([Coluna], JogadasVitoria),
         ! % se houver uma jogada que leva à vitória imediata, retorne essa jogada
@@ -336,8 +401,8 @@ jogada_vencedora_v2(Tabuleiro, Altura, Largura, Consecutivas, Linha, Coluna) :-
 
          findall([C],
                 (member(C, JogadasValidas),
-                 troca_v2(Tabuleiro, C, 'O', NovoTabuleiro, L),
-                 pontuacao_jogada(NovoTabuleiro, Altura, Largura, Consecutivas, L, C, JogadasValidas, P)),
+                 troca_v2(C, 'O', NovoTabuleiro, L),
+                 pontuacao_jogada(NovoTabuleiro, L, C, JogadasValidas, P)),
                 JogadasPontuadas),
                 
         sort(JogadasPontuadas, SortedJogadasPontuadas),
@@ -345,7 +410,12 @@ jogada_vencedora_v2(Tabuleiro, Altura, Largura, Consecutivas, Linha, Coluna) :-
         member([Coluna], ReverseJogadasPontuadas)
     ).
 
-pontuacao_jogada(Tabuleiro, Altura, Largura, Consecutivas, Linha, Coluna, JogadasValidas, Pontuacao) :-
+
+pontuacao_jogada(Tabuleiro, Linha, Coluna, JogadasValidas, Pontuacao) :-
+    altura(Altura),
+    Largura is Altura + 1,
+    Consecutivas is Altura + 1,
+
     troca(Tabuleiro, Linha, Coluna, NovoTabuleiro, 'O'),
     findall(P,
             (member([L, C], JogadasValidas),
